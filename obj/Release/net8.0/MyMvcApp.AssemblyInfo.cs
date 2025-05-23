82b41ff7e299e71cd8b5506c6afb40cc23b15800
github-cloudlabsuser-1399 <168125725+github-cloudlabsuser-1399@users.noreply.github.com>
2025-05-23 13:24:56 +0000

    Add static web assets MSBuild props for MyMvcApp
    - Created msbuild.build.MyMvcApp.props to import Microsoft.AspNetCore.StaticWebAssets.props
    - Created msbuild.buildMultiTargeting.MyMvcApp.props to import MyMvcApp.props for multi-targeting
    - Created msbuild.buildTransitive.MyMvcApp.props to import MyMvcApp.props for transitive builds

--- obj/Release/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Release/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0498d215878ee6b26493691a62aeae3e51f1b52")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
